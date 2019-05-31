6308ac7a667c0005c3c3b94b44c5a20b0feda25c
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-05-31 20:44:22 +0530

    add dropdown

--- src/Checkbox.java
+++ src/Checkbox.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
 
-public class checkbox {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class Checkbox {
+    public static void display() {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("checkbox");
+        window.setMinWidth(250);
+
+        CheckBox a = new CheckBox("Tomato");
+        CheckBox b = new CheckBox("Cucumber");
+        b.setSelected(true);
+
+
+        Button ok = new Button("ok");
+        ok.setOnAction(e -> {
+            handleOptions(a, b);
+
+        });
+        VBox layout = new VBox();
+        layout.getChildren().addAll(a, b, ok);
+
+
+        Scene scene = new Scene(layout, 300, 300);
+        window.setScene(scene);
+        window.showAndWait();
+
+    }
+    private static void handleOptions(CheckBox a, CheckBox b) {
+        String message = "order:";
+        if(a.isSelected()) {
+            message += "tomato";
+        }
+        if(b.isSelected()) {
+            message += "cucumber";
+        }
+
+        System.out.print(message);
+
+    }
+
 }
+
---
+++ src/Dropdown.java
@@ -0,0 +1,54 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import java.awt.*;
+
+public class Dropdown {
+
+    public static void display() {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+
+        window.setMinWidth(250);
+
+
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+
+        choiceBox.getItems().addAll("apple", "mango", "peach", "cherry", "pineapple");
+        choiceBox.setValue("apple");
+
+        Button cb = new Button("X");
+        cb.setOnAction(e -> {
+            getChoice(choiceBox);
+
+        });
+
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(choiceBox, cb);
+        layout.setAlignment(Pos.CENTER);
+
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
+    }
+    private static void getChoice(ChoiceBox<String> choiceBox) {
+        String fruit  = choiceBox.getValue();
+        System.out.print(fruit);
+    }
+}
